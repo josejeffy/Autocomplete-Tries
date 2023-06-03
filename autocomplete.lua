361fd1d56ff1100a13d1940afd7c1f59b90e2abb
Jeffy Joseph <josejeffy@gmail.com>
2023-06-03 17:16:52 +1000

    Update autocomplete.lua

--- autocomplete.lua
+++ autocomplete.lua
@@ -1,63 +1,64 @@
 local Autocomplete = {}
-Autocomplete.root = {}
 
+-- init
 function Autocomplete:init(filename)
-  local file = io.open(filename, 'r')
-  if file then
-    local contents = string.lower(file:read('*all'))
-    file:close()
-    for word in contents:gmatch("([^\n]*)\n?") do
-      Autocomplete:_add(word)
-    end
-  else
-    error('File does not exist')
-  end
-  return self
+	self.suggestions = {}
+	self.root = {}
+	local file = io.open(filename, 'r')
+	if file then
+		local contents = file:read('*all')
+		contents = string.lower(contents)
+		for word in contents:gmatch('([^\n]*)\n?') do
+			self:add(word)
+		end
+	else
+		error('File not found')
+	end
+	return self
 end
 
-function Autocomplete:_clear_suggestions()
-  self.suggestions = {}
+-- add words to trie
+function Autocomplete:add(word)
+	local node = self.root
+	for letter in word:gmatch('.') do
+		if not node[letter] then
+			node[letter] = {}
+		end
+		node = node[letter]
+	end
+	node['is_end'] = true
 end
 
-function Autocomplete:_add(word)
-  local node = self.root
-  for letter in word:gmatch('.') do
-    if not node[letter] then
-      node[letter] = {}
-    end
-    node = node[letter]
-  end
-  node['is_end'] = true
+-- retrieve sub-trie
+function Autocomplete:get_subtrie(word)
+	local node = self.root
+	for letter in word:gmatch('.') do
+		if not node[letter] then
+			return node
+		end
+		node = node[letter]
+	end
+	return node
 end
 
-function Autocomplete:_get_subtrie(word)
-  local node = self.root
-  for letter in word:gmatch('.') do
-    if not node[letter] then
-      return node
-    end
-    node = node[letter]
-  end
-  return node
+-- build suggestions from sub-trie
+function Autocomplete:build_suggestions(node, prefix)
+	for letter, sub_node in pairs(node) do
+		if type(sub_node) == 'table' then
+			local new_prefix = prefix .. letter
+			self:build_suggestions(sub_node, new_prefix)
+		else
+			table.insert(self.suggestions, prefix)
+		end
+	end
 end
 
-
-function Autocomplete:_generate_suggestions(node, prefix)
-  for letter, new_node in pairs(node) do
-    if type(new_node) == 'table' then
-      Autocomplete:_generate_suggestions(new_node, prefix .. letter)
-    else
-      table.insert(self.suggestions, prefix)
-    end
-  end
+-- generate suggestions from search term
+function Autocomplete:generate_suggestions(search_term)
+	self.suggestions = {}
+	local subtrie = self:get_subtrie(search_term)
+	self:build_suggestions(subtrie, search_term)
+	return self.suggestions
 end
-
-function Autocomplete:complete(search_term)
-  Autocomplete:_clear_suggestions()
-  local node = Autocomplete:_get_subtrie(search_term)
-  Autocomplete:_generate_suggestions(node, search_term)
-  return Autocomplete.suggestions
-end
-
 
 return Autocomplete
