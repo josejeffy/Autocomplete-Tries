96b4d1000c9534f992b6fd0e8e9d40e4d413c4f2
Jeffy Joseph <josejeffy@gmail.com>
2023-06-03 11:57:59 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,9 +4,6 @@ autocomplete = Autocomplete:init('unix-words.txt')
 local search_terms = {'cloni','marks','stew'}
 
 for _, search_term in pairs(search_terms) do
-	print(
-		search_term .. 
-		' => ' .. 
-		table.concat(autocomplete:complete(search_term),", ")
-	)
+	local suggestions = autocomplete:complete(search_term)
+	print(search_term .. ' => ' .. table.concat(suggestions,", "))
 end
