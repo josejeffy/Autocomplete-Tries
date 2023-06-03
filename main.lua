1016f63e2d6cf30f50eff40e25966d361c7a7a4f
Jeffy Joseph <josejeffy@gmail.com>
2023-06-03 17:48:48 +1000

    Delete main.lua

--- main.lua
+++
@@ -1,9 +0,0 @@
-local Autocomplete = require 'autocomplete'
-
-autocomplete = Autocomplete:init('unix-words.txt')
-local search_terms = {'cloni','marks','stew'}
-
-for _, search_term in pairs(search_terms) do
-	local suggestions = autocomplete:generate_suggestions(search_term)
-	print(search_term .. ' => ' .. table.concat(suggestions,", "))
-end
